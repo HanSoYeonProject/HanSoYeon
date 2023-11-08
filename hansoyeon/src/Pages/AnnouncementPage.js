8e0e6e71ef91ead49152a4db867ee048a87fc22e
King <tmdwns563@naver.com>
2023-11-08 10:43:15 +0900

    app.js 라우터추가

---
+++ hansoyeon/src/Pages/AnnouncementPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+const AnnouncementPage = (props) => {
+    return (
+        <Container>12</Container>
+    );
+};
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+background-color: blue;
+`
+
+export default AnnouncementPage;
---
+++ hansoyeon/src/Pages/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+const MainPage = (props) => {
+    return (
+    <Container>123</Container>
+    );
+};
+
+const Container = styled.div`
+background-color: green;
+  width:100%;
+  height: 100vh;
+`
+
+export default MainPage;
