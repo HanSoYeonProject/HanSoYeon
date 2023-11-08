b7ada021195d5409b0c92c6c81e3b2a6cdf0f483
King <tmdwns563@naver.com>
2023-11-09 01:09:16 +0900

    UpNav

--- hansoyeon/src/App.js
+++ hansoyeon/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navigate from "./Components/Navigate";
 import MainPage from "./Pages/MainPage";
 import AnnouncementPage from "./Pages/AnnouncementPage";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navigate/>
         <Routes>
           <Route path = "/" element={<MainPage/>}></Route>
             <Route path ="/Announcement" element={<AnnouncementPage/>}></Route>
--- hansoyeon/src/Pages/MainPage.js
+++ hansoyeon/src/Pages/MainPage.js
@@ -2,12 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 const MainPage = (props) => {
     return (
-    <Container>123</Container>
+    <Container></Container>
     );
 };
 
 const Container = styled.div`
-background-color: green;
   width:100%;
   height: 100vh;
 `
