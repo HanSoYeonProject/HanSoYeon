19a7704d00f1c7ab74c1249ee4d960fbc487d753
gwanghuiLee <lgh970409@gmail.com>
2023-11-10 22:48:58 +0900

    nav_page_add

--- hansoyeon/src/App.js
+++ hansoyeon/src/App.js
@@ -6,6 +6,16 @@ import MainPage from "./Pages/MainPage";
 import AnnouncementPage from "./Pages/AnnouncementPage";
 import LoginPage from "./Pages/SigninPage"
 import RegisterPage from "./Pages/SignupPage"
+import NewPage from "./Pages/NewPage";
+import RecommendPage from "./Pages/RecommendPage";
+import ThemePage from "./Pages/ThemePage";
+import RegionPage from "./Pages/RegionPage";
+import ReviewPage from "./Pages/ReviewPage";
+import NoticePage from "./Pages/NoticePage";
+
+function RecommendCourse() {
+    return null;
+}
 
 function App() {
   return (
@@ -17,6 +27,14 @@ function App() {
             <Route path ="/Announcement" element={<AnnouncementPage/>}></Route>
             <Route path = "/login" element={<LoginPage />}></Route>
             <Route path = "/register" element={<RegisterPage />}></Route>
+
+            <Route path = "/newcourse" element={<NewPage />}></Route>
+            <Route path = "/recommendcourse" element={<RecommendPage />}></Route>
+            <Route path = "/themecourse" element={<ThemePage />}></Route>
+            <Route path = "/regioncourse" element={<RegionPage />}></Route>
+            <Route path = "/review" element={<ReviewPage />}></Route>
+            <Route path = "/notice" element={<NoticePage />}></Route>
+
         </Routes>
       </BrowserRouter>
     </div>
---
+++ hansoyeon/src/Pages/NoticePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+const NoticePage = (props) => {
+    return (
+        <Container></Container>
+    );
+};
+
+const Container = styled.div`
+  width:100%;
+  height: 100vh;
+`
+
+export default NoticePage;
