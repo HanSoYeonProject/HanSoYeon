4d9f76ab87bc48a9b8c06cb331c9e3666268adbd
kriey1 <dongjun569@naver.com>
2023-11-10 22:31:09 +0900

    구글지도 연동
    .env로 구글지도

--- hansoyeon/src/Pages/MainPage.js
+++ hansoyeon/src/Pages/MainPage.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
-const MainPage = (props) => {
-    return (
-    <Container></Container>
-    );
+import { useJsApiLoader, GoogleMap, Marker} from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
 };
 
-const Container = styled.div`
-  width:100%;
-  height: 100vh;
-`
+const MAP_API_KEY = process.env.GOOGLE_MAP_API_KEY;
+
+function GoogleMapComponent() {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "${MAP_API_KEY}" // Replace with your API key
+    });
+
+    const [map, setMap] = useState(null);
+    const [currentLocation, setCurrentLocation] = useState(null);
+
+    const onLoad = useCallback(function callback(map) {
+        setMap(map);
+    }, []);
+
+    const onUnmount = useCallback(function callback(map) {
+        setMap(null);
+    }, []);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                setCurrentLocation({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                });
+            },
+            function(error) {
+                console.error("Error getting location", error);
+            },
+            {
+                enableHighAccuracy: true
+            }
+        );
+    }, []);
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={currentLocation || { lat: 37.5760222, lng: 126.9769000 }}
+            zoom={16}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+        >
+            {currentLocation && <Marker position={currentLocation} />}
+        </GoogleMap>
+    ) : <></>;
+}
+
+
+const MainPage = () => {
+    return (
+        <div>
+            <GoogleMapComponent />
+        </div>
+    );
+};
 
 export default MainPage;
