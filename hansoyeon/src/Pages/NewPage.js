0da467c7928f1c966d72939dbe05e09c65a25b96
YangjiwooGN <jwoo3460@gmail.com>
2023-11-22 16:06:08 +0900

    feat)update_local

--- hansoyeon/src/Pages/NewPage.js
+++ hansoyeon/src/Pages/NewPage.js
@@ -11,12 +11,177 @@ const NewPage = (props) => {
     const [touristSpots, setTouristSpots] = useState([]);
     const [keyword, setKeyword] = useState("");
     const [searchKeyword, setSearchKeyword] = useState("");
+    const [searchDetailKeyword, setSearchDetailKeyword] = useState("");
     const [pageNo, setPageNo] = useState(1);
+
+    const [selectedSido, setSelectedSido] = useState('');
+    const [selectedGugun, setSelectedGugun] = useState('');
+    const [gugunOptions, setGugunOptions] = useState([]);
+    const [localNum, setLocalNum] = useState("1");
+    const [detailNum, setDetailNum] = useState("1");
+
+    const sidoOptions = [
+        "시/도 선택", "서울특별시", "인천광역시", "대전광역시", "광주광역시",
+        "대구광역시", "부산광역시", "경기도", "강원도", "충청북도",
+        "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도"
+    ];
+
+    useEffect(() => {
+        let option = [];
+        if (!selectedSido || selectedSido === "시/도 선택") {
+            setGugunOptions([]);
+            setSelectedGugun('');
+        } else {
+            if (selectedSido === "서울특별시") {
+                setLocalNum("1")
+                option = [
+                    {name: "강남구", detailNum: "1"}, {name: "강동구", detailNum: "2"}, {name: "강북구", detailNum: "3"},
+                    {name: "강서구", detailNum: "4"}, {name: "관악구", detailNum: "5"}, {name: "광진구", detailNum: "6"},
+                    {name: "구로구", detailNum: "7"}, {name: "금천구", detailNum: "8"}, {name: "노원구", detailNum: "9"},
+                    {name: "도봉구", detailNum: "10"}, {name: "동대문구", detailNum: "11"}, {name: "동작구", detailNum: "12"},
+                    {name: "마포구", detailNum: "13"}, {name: "서대문구", detailNum: "14"}, {name: "서초구", detailNum: "15"},
+                    {name: "성동구", detailNum: "16"}, {name: "성북구", detailNum: "17"}, {name: "송파구", detailNum: "18"},
+                    {name: "양천구", detailNum: "19"}, {name: "영등포구", detailNum: "20"}, {name: "용산구", detailNum: "21"},
+                    {name: "은평구", detailNum: "22"}, {name: "종로구", detailNum: "23"}, {name: "중구", detailNum: "24"}, {name: "중랑구", detailNum: "25"}
+                ]
+            }else if(selectedSido === "인천광역시"){
+                setLocalNum("2")
+                option = [
+                    {name: "계양구", detailNum: "2"}, {name: "미추홀구", detailNum: "3"}, {name: "남동구", detailNum: "4"},
+                    {name: "동구", detailNum: "5"}, {name: "부평구", detailNum: "6"}, {name: "서구", detailNum: "7"},
+                    {name: "연수구", detailNum: "8"}, {name: "중구", detailNum: "10"}, {name: "강화군", detailNum: "1"}, {name: "옹진군", detailNum: "9"}
+                ]
+            }else if(selectedSido === "대전광역시"){
+                setLocalNum("3")
+                option = [
+                    {name: "대덕구", detailNum: "1"}, {name: "동구", detailNum: "2"}, {name: "서구", detailNum: "3"},
+                    {name: "유성구", detailNum: "4"}, {name: "중구", detailNum: "5"}
+                ]
+            }else if(selectedSido === "광주광역시"){
+                setLocalNum("5")
+                option = [
+                    {name: "광산구", detailNum: "1"}, {name: "남구", detailNum: "2"}, {name: "동구", detailNum: "3"},
+                    {name: "북구", detailNum: "4"}, {name: "서구", detailNum: "5"}
+                ]
+            }else if(selectedSido === "대구광역시"){
+                setLocalNum("4")
+                option = [
+                    {name: "남구", detailNum: "1"}, {name: "달서구", detailNum: "2"}, {name: "동구", detailNum: "4"},
+                    {name: "북구", detailNum: "5"}, {name: "서구", detailNum: "6"}, {name: "수성구", detailNum: "7"},
+                    {name: "중구", detailNum: "8"}, {name: "달성군", detailNum: "3"}, {name: "군위군", detailNum: "9"}
+                ]
+            }else if(selectedSido === "울산광역시"){
+                setLocalNum("7")
+                option = [
+                    {name: "남구", detailNum: "2"}, {name: "동구", detailNum: "3"}, {name: "북구", detailNum: "4"},
+                    {name: "중구", detailNum: "1"}, {name: "울주군", detailNum: "5"}
+                ]
+            }else if(selectedSido === "부산광역시"){
+                setLocalNum("6")
+                option = [
+                    {name: "강서구", detailNum: "1"}, {name: "금정구", detailNum: "2"}, {name: "남구", detailNum: "4"},
+                    {name: "동구", detailNum: "5"}, {name: "동래구", detailNum: "6"}, {name: "부산진구", detailNum: "7"},
+                    {name: "북구", detailNum: "8"}, {name: "사상구", detailNum: "9"}, {name: "사하구", detailNum: "10"},
+                    {name: "서구", detailNum: "11"}, {name: "수영구", detailNum: "12"}, {name: "연제구", detailNum: "13"},
+                    {name: "영도구", detailNum: "14"}, {name: "중구", detailNum: "15"}, {name: "해운대구", detailNum: "16"}, {name: "기장군", detailNum: "3"}
+                ]
+            }else if(selectedSido === "경기도"){
+                setLocalNum("31")
+                option = [
+                    {name: "고양시", detailNum: "2"}, {name: "과천시", detailNum: "3"}, {name: "광명시", detailNum: "4"},
+                    {name: "광주시", detailNum: "5"}, {name: "구리시", detailNum: "6"}, {name: "군포시", detailNum: "7"},
+                    {name: "김포시", detailNum: "8"}, {name: "남양주시", detailNum: "9"}, {name: "동두천시", detailNum: "10"},
+                    {name: "부천시", detailNum: "11"}, {name: "성남시", detailNum: "12"}, {name: "수원시", detailNum: "13"},
+                    {name: "시흥시", detailNum: "14"}, {name: "안산시", detailNum: "15"}, {name: "안성시", detailNum: "16"}, {name: "안양시", detailNum: "17"},
+                    {name: "양주시", detailNum: "18"}, {name: "오산시", detailNum: "22"}, {name: "용인시", detailNum: "23"},
+                    {name: "의왕시", detailNum: "24"}, {name: "의정부시", detailNum: "25"}, {name: "이천시", detailNum: "26"},
+                    {name: "파주시", detailNum: "27"}, {name: "평택시", detailNum: "28"}, {name: "포천시", detailNum: "29"},
+                    {name: "하남시", detailNum: "30"}, {name: "화성시", detailNum: "31"}, {name: "가평군", detailNum: "1"},
+                    {name: "양평군", detailNum: "19"}, {name: "여주시", detailNum: "20"}, {name: "연천군", detailNum: "21"},
+                ]
+            }else if(selectedSido === "강원도"){
+                setLocalNum("32")
+                option = [
+                    {name: "강릉시", detailNum: "1"}, {name: "고성군", detailNum: "2"}, {name: "동해시", detailNum: "3"},
+                    {name: "삼척시", detailNum: "4"}, {name: "속초시", detailNum: "5"}, {name: "양구군", detailNum: "6"},
+                    {name: "양양군", detailNum: "7"}, {name: "영월군", detailNum: "8"}, {name: "원주시", detailNum: "9"},
+                    {name: "인제군", detailNum: "10"}, {name: "정선군", detailNum: "11"}, {name: "철원군", detailNum: "12"},
+                    {name: "춘천시", detailNum: "13"}, {name: "태백시", detailNum: "14"}, {name: "평창군", detailNum: "15"},
+                    {name: "홍천군", detailNum: "16"}, {name: "화천군", detailNum: "17"}, {name: "횡성군", detailNum: "18"}
+                ]
+            }else if(selectedSido === "충청북도"){
+                setLocalNum("33")
+                option = [
+                    {name: "괴산군", detailNum: "1"}, {name: "단양군", detailNum: "2"}, {name: "보은군", detailNum: "3"},
+                    {name: "영동군", detailNum: "4"}, {name: "옥천군", detailNum: "5"}, {name: "음성군", detailNum: "6"},
+                    {name: "제천시", detailNum: "7"}, {name: "진천군", detailNum: "8"}, {name: "청원군", detailNum: "9"},
+                    {name: "청주시", detailNum: "10"}, {name: "충주시", detailNum: "11"}, {name: "증평군", detailNum: "12"}
+                ]
+            }else if(selectedSido === "충청남도"){
+                setLocalNum("34")
+                option = [
+                    {name: "공주시", detailNum: "1"}, {name: "금산군", detailNum: "2"}, {name: "논산시", detailNum: "3"},
+                    {name: "당진시", detailNum: "4"}, {name: "보령시", detailNum: "5"}, {name: "부여군", detailNum: "6"},
+                    {name: "서산시", detailNum: "7"}, {name: "서천군", detailNum: "8"}, {name: "아산시", detailNum: "9"},
+                    {name: "예산군", detailNum: "10"}, {name: "천안시", detailNum: "11"}, {name: "청양군", detailNum: "12"},
+                    {name: "태안군", detailNum: "13"}, {name: "홍성군", detailNum: "14"}, {name: "계룡시", detailNum: "15"}
+                ]
+            }else if(selectedSido === "전라북도"){
+                setLocalNum("37")
+                option = [
+                    {name: "고창군", detailNum: "1"}, {name: "군산시", detailNum: "2"}, {name: "김제시", detailNum: "3"},
+                    {name: "남원시", detailNum: "4"}, {name: "무주군", detailNum: "5"}, {name: "부안군", detailNum: "6"},
+                    {name: "순창군", detailNum: "7"}, {name: "완주군", detailNum: "8"}, {name: "익산시", detailNum: "9"},
+                    {name: "임실군", detailNum: "10"}, {name: "장수군", detailNum: "11"}, {name: "전주시", detailNum: "12"},
+                    {name: "정읍시", detailNum: "13"}, {name: "진안군", detailNum: "14"}
+                ]
+            }else if(selectedSido === "전라남도"){
+                setLocalNum("38")
+                option = [
+                    {name: "강진군", detailNum: "1"}, {name: "고흥군", detailNum: "2"}, {name: "곡성군", detailNum: "3"},
+                    {name: "광양시", detailNum: "4"}, {name: "구례군", detailNum: "5"}, {name: "나주시", detailNum: "6"},
+                    {name: "담양군", detailNum: "7"}, {name: "목포시", detailNum: "8"}, {name: "무안군", detailNum: "9"},
+                    {name: "보성군", detailNum: "10"}, {name: "순천시", detailNum: "11"}, {name: "신안군", detailNum: "12"},
+                    {name: "여수시", detailNum: "13"}, {name: "영광군", detailNum: "16"}, {name: "영암군", detailNum: "17"},
+                    {name: "완도군", detailNum: "18"}, {name: "장성군", detailNum: "19"}, {name: "장흥군", detailNum: "20"},
+                    {name: "진도군", detailNum: "21"}, {name: "함평군", detailNum: "22"}, {name: "해남군", detailNum: "23"}, {name: "화순군", detailNum: "24"}
+                ]
+            }else if(selectedSido === "경상북도"){
+                setLocalNum("35")
+                option = [
+                    {name: "경산시", detailNum: "1"}, {name: "경주시", detailNum: "2"}, {name: "고령군", detailNum: "3"},
+                    {name: "구미시", detailNum: "4"}, {name: "김천시", detailNum: "6"}, {name: "문경시", detailNum: "7"},
+                    {name: "봉화군", detailNum: "8"}, {name: "상주시", detailNum: "9"}, {name: "성주군", detailNum: "10"},
+                    {name: "안동시", detailNum: "11"}, {name: "영덕군", detailNum: "12"}, {name: "영양군", detailNum: "13"},
+                    {name: "영주시", detailNum: "14"}, {name: "영천시", detailNum: "15"}, {name: "예천군", detailNum: "16"},
+                    {name: "울릉군", detailNum: "17"}, {name: "울진군", detailNum: "18"}, {name: "의성군", detailNum: "19"},
+                    {name: "청도군", detailNum: "20"}, {name: "청송군", detailNum: "21"}, {name: "칠곡군", detailNum: "22"}, {name: "포항시", detailNum: "23"}
+                ]
+            }else if(selectedSido === "경상남도"){
+                setLocalNum("36")
+                option = [
+                    {name: "거제시", detailNum: "1"}, {name: "거창군", detailNum: "2"}, {name: "고성군", detailNum: "3"},
+                    {name: "김해시", detailNum: "4"}, {name: "남해군", detailNum: "5"}, {name: "마산시", detailNum: "6"},
+                    {name: "밀양시", detailNum: "7"}, {name: "사천시", detailNum: "8"}, {name: "산청군", detailNum: "9"},
+                    {name: "양산시", detailNum: "10"}, {name: "의령군", detailNum: "12"}, {name: "진주시", detailNum: "13"},
+                    {name: "진해시", detailNum: "14"}, {name: "창녕군", detailNum: "15"}, {name: "창원시", detailNum: "16"},
+                    {name: "통영시", detailNum: "17"}, {name: "하동군", detailNum: "18"}, {name: "함안군", detailNum: "19"},
+                    {name: "함양군", detailNum: "20"}, {name: "합천군", detailNum: "21"}
+                ]
+            }else if(selectedSido === "제주특별자치도"){
+                setLocalNum("39")
+                option = [
+                    {name: "남제주군", detailNum: "1"}, {name: "북제주군", detailNum: "2"}, {name: "서귀포시", detailNum: "3"}, {name: "제주시", detailNum: "4"}
+                ]
+            }
+            setGugunOptions(option);
+        }
+    }, [selectedSido]);
 
     const serviceKey = 'YRUALCBQQWvU6w/tG7ZkUtWtjAeaO9bJjyummGjvfF9SjR0QYO+CRveierZlwe97v5toXybLb6aoFCl1sZ8q4Q==';
 
     useEffect(() => {
-        fetchData(keyword, pageNo);
+        fetchLocalData(keyword, pageNo);
     }, [pageNo]);
 
     const fetchData = async (searchKeyword, pageNo, retryCount = 0) => {
@@ -49,6 +214,46 @@ const NewPage = (props) => {
         }
     };
 
+    const fetchLocalData = async (searchKeyword, pageNo, retryCount = 0) => {
+        try {
+            const encodedServiceKey = encodeURIComponent(serviceKey);
+            const url = `http://apis.data.go.kr/B551011/KorService1/areaBasedList1?serviceKey=${encodedServiceKey}&pageNo=${pageNo}&numOfRows=12&MobileApp=AppTest&MobileOS=ETC&arrange=A&areaCode=${localNum}&sigunguCode=${detailNum}`;
+            const response = await axios.get(url);
+
+            const parser = new XMLParser();
+            const jsonObj = parser.parse(response.data);
+
+            if (jsonObj && jsonObj.response && jsonObj.response.body && jsonObj.response.body.items) {
+                const spots = jsonObj.response.body.items.item;
+                setTouristSpots(spots);
+                setSearchKeyword(searchKeyword)
+                setSearchDetailKeyword(selectedGugun);
+            } else {
+                // 유효하지 않은 응답일 경우 재시도
+                if (retryCount < 3) { // 최대 3번 재시도
+                    fetchLocalData(searchKeyword, pageNo, retryCount + 1);
+                } else {
+                    console.error("Invalid API response");
+                }
+            }
+        } catch (error) {
+            console.error('Error fetching data: ', error);
+            if (retryCount < 3) {
+                fetchLocalData(searchKeyword, pageNo, retryCount + 1);
+            }
+        }
+    };
+
+    // 구/군 선택 핸들러
+    const handleGugunChange = (e) => {
+        const selectedOption = gugunOptions.find(option => option.name === e.target.value);
+        setSelectedGugun(e.target.value);
+        if (selectedOption) {
+            setDetailNum(selectedOption.detailNum);
+        } else {
+            setDetailNum("");
+        }
+    };
 
     const handleTabClick = (tabNumber) => {
         setActiveTab(tabNumber);
@@ -159,16 +364,25 @@ const NewPage = (props) => {
                         <AreaContentContainer>
                             <SearchBoxContainer>
                                 <SearchContainer>
-                                    <SearchInput
-                                        type="text"
-                                        value={keyword}
-                                        onChange={(e) => setKeyword(e.target.value)}
-                                        placeholder="지역 검색"
-                                    />
-                                    <SearchButton onClick={() => fetchData(keyword, 1)}>검색</SearchButton>
+                                    {/* 시/도 선택 드롭다운 */}
+                                    <StyledSelect value={selectedSido} onChange={(e) => setSelectedSido(e.target.value)}>
+                                        {sidoOptions.map(sido => (
+                                            <option key={sido} value={sido}>{sido}</option>
+                                        ))}
+                                    </StyledSelect>
+
+                                    {/* 구/군 선택 드롭다운 */}
+                                    <StyledSelect value={selectedGugun} onChange={handleGugunChange}>
+                                        {gugunOptions.map(({ name }) => (
+                                            <option key={name} value={name}>{name}</option>
+                                        ))}
+                                    </StyledSelect>
+
+                                    {/* 검색 버튼 */}
+                                    <SearchButton onClick={() => fetchLocalData(selectedSido, selectedGugun, 1)}>검색</SearchButton>
                                 </SearchContainer>
                                 <SearchResultText>
-                                    {searchKeyword ? `'${searchKeyword}' 검색 결과입니다.` : '전체 목록'}
+                                    {searchKeyword ? `'${searchKeyword} ${searchDetailKeyword}' 검색 결과입니다.` : '전체 목록'}
                                 </SearchResultText>
                             </SearchBoxContainer>
                             <GridContainer>
@@ -550,6 +764,20 @@ const SearchResultText = styled.div`
   margin-bottom: 10px;
   color: #333;
   font-size: 17px;
+`;
+
+const StyledSelect = styled.select`
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  background-color: white;
+  box-shadow: 0px 2px 4px rgba(0,0,0,0.1);
+  margin-right: 10px;
+  cursor: pointer;
+
+  &:hover {
+    border-color: #aaa;
+  }
 `;
 
 export default NewPage;
