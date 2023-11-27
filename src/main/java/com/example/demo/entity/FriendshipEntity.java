937c6e2cf10fce133c5f6d5de06cdaaa83dd077f
woody35545 <woody35545@gmail.com>
2023-11-28 01:12:50 +0900

    Loombok 어노테이션 추가

--- src/main/java/com/example/demo/entity/FriendshipEntity.java
+++ src/main/java/com/example/demo/entity/FriendshipEntity.java
@@ -1,8 +1,17 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "friendships")
 public class FriendshipEntity {
 
