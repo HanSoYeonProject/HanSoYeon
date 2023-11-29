60abb01d3d781c0a773847d1537045bb23a1475c
King <tmdwns563@naver.com>
2023-11-30 00:39:59 +0900

    상세페이지 및 드롭다운

--- hansoyeon/src/Pages/RecruitViewPage.js
+++ hansoyeon/src/Pages/RecruitViewPage.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import {useNavigate, useParams} from 'react-router-dom'; // useNavigate 훅 추가
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
 import axios from "axios";
+import styled from "styled-components";
+import about1 from "../imgs/about1.png";
+import about2 from "../imgs/about2.png";
+import about3 from "../imgs/about3.png";
+import about4 from "../imgs/about4.jpg";
+import location from "../imgs/location.png";
 
 const RecruitViewPage = ( props ) => {
     const { id } = useParams();
@@ -33,9 +35,79 @@ const RecruitViewPage = ( props ) => {
 
     return (
         <Container>
-
+            <ImgContainer>
+                <ImgSmallContainer>
+                    <img src = {about1}/>
+                    <img src = {about2}/>
+                    <img src = {about3}/>
+                    <img src = {about4}/>
+                </ImgSmallContainer>
+            </ImgContainer>
+            <LocationContainer>
+                <h2><img src = {location}/> {` ${recruitments.region}`}</h2>
+            </LocationContainer>
+            <TitleContainer>
+                <h2>{recruitments.title}</h2>
+                {/*<h2>{recruitments.providers}</h2>*/}
+            </TitleContainer>
+            <ApplyButton>
+                지원하기
+            </ApplyButton>
         </Container>
     );
 };
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
+
+const ImgContainer = styled.div`
+  display: flex;
+  height: 300px;
+  width: 100%;
+  margin-top: 10rem;
+  justify-content: center;
+`
+
+const ImgSmallContainer = styled.div`
+  display: flex;
+  width: 98%;
+  flex-direction: row;
+  
+  img {
+    width: 20%;
+    height: auto;
+    margin: 0 auto;
+  }
+`
+
+const LocationContainer = styled.div`
+  display: flex;
+  margin-top: 2rem;
+  align-items: center;
+  h2 {
+    margin-left: 5rem;
+    font-size: 20px;
+    color: orangered;
+  }
+  
+  img {
+    height: 16px;
+    
+  }
+`
+const TitleContainer = styled.div`
+  display: flex;
+  height: auto;
+  margin-top: 1rem;
+  
+  h2 {
+    margin-left: 5rem;
+    font-size: 32px;
+  }
+`
+const ApplyButton = styled.button`
+`
 export default RecruitViewPage;
