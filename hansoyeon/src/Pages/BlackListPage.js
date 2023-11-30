b67ffeb638e77b66a650761f5e5bab5ad2dd1ef8
kriey1 <96155832+kriey1@users.noreply.github.com>
2023-11-30 17:14:57 +0900

    Revert "Feat/블랙리스트 기업정보 출력"

--- hansoyeon/src/Pages/BlackListPage.js
+++ hansoyeon/src/Pages/BlackListPage.js
@@ -1,56 +1,90 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import styled from "styled-components";
-import { Table } from "react-bootstrap";
-import {useNavigate} from "react-router-dom";
+import {Table} from "react-bootstrap";
 
 const BlackListPage = () => {
-    const navigate = useNavigate();
     const [blacklist, setBlacklist] = useState([]);
-    const {user, setUser} = useState();
-    const [provider, setProviderId] = useState();
+    const [activeTab, setActiveTab] = useState(1); // 탭 상태 관리
 
     useEffect(() => {
-        fetchBlacklist(user, provider);
-    }, []);
+        fetchBlacklist();
+    }, [activeTab]); // activeTab 변경 시 fetchBlacklist 호출
 
     const fetchBlacklist = () => {
-        const url = "http://localhost:8050/api/blacklist";
-        axios
-            .get(url)
-            .then((response) => {
+        // 탭에 따라 다른 URL에서 데이터를 가져올 수 있음
+        const url = activeTab === 1 ? "http://localhost:8050/api/blacklist/specific" : "http://localhost:8050/api/blacklist/all";
+        axios.get(url)
+            .then(response => {
                 setBlacklist(response.data);
             })
-            .catch((error) => {
+            .catch(error => {
                 console.error("Error fetching blacklist: ", error);
             });
     };
+
+    const handleTabClick = (tabNumber) => {
+        setActiveTab(tabNumber);
+    };
+
+
 
     return (
         <Container>
             <Header>블랙리스트 관리</Header>
 
-            <StyledTable striped bordered hover>
-                <thead>
-                <tr>
-                    <th>회원명</th>
-                    <th>기업명</th>
-                    <th>아이디</th>
-                    <th>전화번호</th>
-                    <RotatedTextHeader>삭제</RotatedTextHeader>
-                </tr>
-                </thead>
-                <tbody>
-                {blacklist.map((user, index) => (
-                    <tr key={index}>
-                        <td>{user.user.nameId}</td>
-                        <td>{user.id}</td>
-                        <td>{user.phone}</td>
-                        <td> {/* 삭제 버튼 추가 위치 */}</td>
+            <ButtonContainer>
+                <TabButton onClick={() => handleTabClick(1)} className={activeTab === 1 ? 'active' : ''}>특정 공고 사용불가</TabButton>
+                <TabButton onClick={() => handleTabClick(2)} className={activeTab === 2 ? 'active' : ''}>전체 사용불가</TabButton>
+            </ButtonContainer>
+
+            {activeTab === 1&& (
+                <StyledTable striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>회원명</th>
+                        <th>기업명</th>
+                        <th>아이디</th>
+                        <th>전화번호</th>
+                        <th>삭제</th>
                     </tr>
-                ))}
-                </tbody>
-            </StyledTable>
+                    </thead>
+                    <tbody>
+                    {blacklist.map((user, index) => (
+                        <tr key={index}>
+                            <td>{user.name}</td>
+                            <td>{user.id}</td>
+                            <td>{user.phone}</td>
+                            <td> {/* 삭제 버튼 추가 위치 */}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </StyledTable>
+            )}
+
+            {activeTab === 2&& (
+                <StyledTable striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>회원명</th>
+                        <th>아이디</th>
+                        <th>전화번호</th>
+                        <th>삭제</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {blacklist.map((user, index) => (
+                        <tr key={index}>
+                            <td>{user.name}</td>
+                            <td>{user.id}</td>
+                            <td>{user.phone}</td>
+                            <td> {/* 삭제 버튼 추가 위치 */}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </StyledTable>
+            )}
+
         </Container>
     );
 };
@@ -70,6 +104,24 @@ const Header = styled.h2`
   font-weight: bold;
 `;
 
+const TabButton = styled.button`
+  margin: 10px;
+  padding: 10px 20px;
+  background-color: #f8f9fa;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e2e6ea;
+  }
+
+  &.active {
+    background-color: #007bff;
+    color: white;
+  }
+`;
+
 const StyledTable = styled(Table)`
   background-color: white;
   width: 80%;
@@ -78,8 +130,7 @@ const StyledTable = styled(Table)`
     background-color: #f8f9fa;
   }
 
-  th,
-  td {
+  th, td {
     text-align: center;
     vertical-align: middle;
   }
@@ -88,39 +139,37 @@ const StyledTable = styled(Table)`
     font-size: 17px;
   }
 
-  th:first-child,
-  td:first-child {
+  th:first-child, td:first-child {
     width: 250px;
   }
 
-  th:nth-child(2),
-  td:nth-child(2) {
+  th:nth-child(2), td:nth-child(2) {
     width: 250px;
   }
 
-  th:nth-child(3),
-  td:nth-child(3) {
+  th:nth-child(3), td:nth-child(3) {
     width: 300px;
   }
 
-  th:nth-child(4),
-  td:nth-child(4) {
-    width: 160px;
+  th:nth-child(4), td:nth-child(4) {
+    width: 150px;
   }
 
   @media (max-width: 768px) {
     width: 100%;
-    font-size: 14px;
+    font-size: 14px; /* 화면이 작을 때 폰트 크기 줄임 */
 
-    th,
-    td {
-      padding: 10px;
+    th, td {
+      padding: 10px; /* 셀의 패딩 조정 */
     }
+
+    /* 필요한 경우 여기에 더 많은 스타일 규칙 추가 */
   }
 `;
 
-const RotatedTextHeader = styled.th`
-  white-space: nowrap;
+
+const ButtonContainer = styled.div`
+
 `;
 
 export default BlackListPage;
